b397acb45298bdb95ab72b9d9983fa2ec193542a
“KarolyneMachado” <karol_lyne_msl@hotmail.com.br>
2024-08-16 19:52:29 -0300

    update project

--- cypress/e2e/Amazon/cadastro.cy.js
+++ cypress/e2e/Amazon/cadastro.cy.js
@@ -1,16 +1,16 @@
 /// <reference types="cypress" />
 
 describe('Cadastro de usuário na Amazon', () => {
-    beforeEach(() => {
-      cy.visit('https://www.amazon.com.br')
-      cy.get('#nav-link-accountList-nav-line-1').click()
-      cy.get('#createAccountSubmit').click()
-    })
-  
-    it('CT01- Preencher uma conta com informações válidas', () => {
-      cy.get('#ap_customer_name').type('Teste')
-      cy.get('#ap_email').type('teste@example.com')
-      cy.get('#ap_password').type('senha123')
-      cy.get('#ap_password_check').type('senha123')
-    })
+  beforeEach(() => {
+    cy.visit('https://www.amazon.com.br')
+    cy.get('#nav-link-accountList-nav-line-1').click()
+    cy.get('#createAccountSubmit').click()
   })
+
+  it('CT01- Preencher uma conta com informações válidas', () => {
+    cy.get('#ap_customer_name').type('Teste')
+    cy.get('#ap_email').type('teste@example.com')
+    cy.get('#ap_password').type('senha123')
+    cy.get('#ap_password_check').type('senha123')
+  })
+})
